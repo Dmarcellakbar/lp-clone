d9678b872697d54505f58401cf01377bd0d2ecbe
Dmarcellakbar <77231005+Dmarcellakbar@users.noreply.github.com>
2022-10-24 13:44:06 +0700

    meta&change-size-image

--- pages/login.tsx
+++ pages/login.tsx
@@ -43,6 +43,25 @@ export default function CallToActionWithIllustration() {
     <section style={styling}>
       <Head>
         <link rel="shortcut icon" href="/logo-white.png" />
+        <title>The Best Management Firm Any Crypto Investor Can Dream of</title>
+        <meta name="description" content="Our costumized strategy makes crypto investing safer, easier, and more effective for you"/>
+        <meta property="image" content="https://cdn.discordapp.com/attachments/987355942825894028/1033972993854033970/op-image-cfund.png"/>
+        <meta property="url" content="https://cfund.asia"/>
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:title" content="The Best Management Firm Any Crypto Investor Can Dream of" />
+        <meta property="og:description" content="Our costumized strategy makes crypto investing safer, easier, and more effective for you"/>
+        <meta property="og:image" content="https://cdn.discordapp.com/attachments/987355942825894028/1033972993854033970/op-image-cfund.png"/>
+        <meta property="og:url" content="https://cfund.asia"/>
+        <meta property="og:type" content="website"/>
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="cfund.asia"/>
+        <meta property="twitter:url" content="https://cfund.asia"/>
+        <meta name="twitter:title" content="The Best Management Firm Any Crypto Investor Can Dream of"/>
+        <meta name="twitter:description" content="Our costumized strategy makes crypto investing safer, easier, and more effective for you"/>
+        <meta name="twitter:image" content="https://cdn.discordapp.com/attachments/987355942825894028/1033972993854033970/op-image-cfund.png"/>
       </Head>
       <div id="tsparticles">
        <Particle/>
@@ -53,41 +72,40 @@ export default function CallToActionWithIllustration() {
         </Wrap>
       </Link>
       <Crisp/>
-    <Container maxW={'80rem'} height={'100vh'} pt={'8rem'} pb={'8rem'} verticalAlign={'center'}>
-      <VStack
-        textAlign={'center'}
-        align={'center'}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}
-        display={'flex'}
-        >
-        <Heading
-          fontWeight={600}
-          fontSize={'32px'}
-          lineHeight={'110%'}>
-          Login to your account
-        </Heading>
-        <Text color={'#FFF'} fontSize={'16px'}>
-          For now, the user dashboard can only be accessed through the CFund application,
-          which is available in several marketplaces
-          </Text>
-          <HStack spacing='10px' pb={'90px'} >
-              <Box cursor={'pointer'}>
-              <Link href="https://apps.apple.com/sg/app/cfund/id1580696846?l=id" target='_blank'>
-                  <Image width={'150%'} height={'50%'} src={AppStoreBadge}/>
-              </Link>
-              </Box>
-              <Box cursor={'pointer'}>
-              <Link href="https://play.google.com/store/apps/details?id=id.cfund.app&hl=in&gl=US" target='_blank'>
-                  <Image width={'175%'} height={'75%'} src={PlayStoreBadge}/>
-              </Link>
-              </Box>
-          </HStack>
-            <Wrap style={{ position: 'absolute', display: 'flex', flex: '1', bottom: '0', width: '17rem'}}>
-                  <Image src={phone}/>
-            </Wrap>
-      </VStack>
-    </Container>
+      <Container maxW={'80rem'} height={'100vh'} pt={'4rem'} verticalAlign={'center'}>
+        <VStack
+          textAlign={'center'}
+          align={'center'}
+          spacing={'4'}
+          py={'4rem'}
+          display={'flex'}
+          >
+          <Heading
+            fontWeight={600}
+            fontSize={'32px'}>
+            Login to your account
+          </Heading>
+          <Text color={'#FFF'} fontSize={'16px'}>
+            For now, the user dashboard can only be accessed through the CFund application,
+            which is <br/> available in several marketplaces
+            </Text>
+            <HStack spacing='10px' pb={'30rem'} >
+                <Box cursor={'pointer'}>
+                <Link href="https://apps.apple.com/sg/app/cfund/id1580696846?l=id" target='_blank'>
+                    <Image width={'150%'} height={'50%'} src={AppStoreBadge}/>
+                </Link>
+                </Box>
+                <Box cursor={'pointer'}>
+                <Link href="https://play.google.com/store/apps/details?id=id.cfund.app&hl=in&gl=US" target='_blank'>
+                    <Image width={'175%'} height={'75%'} src={PlayStoreBadge}/>
+                </Link>
+                </Box>
+            </HStack>
+              <Wrap style={{ position: 'absolute', display: 'flex', flex: '1', bottom: '0', width: '30rem'}}>
+                    <Image src={phone}/>
+              </Wrap>
+        </VStack>
+      </Container>
     </section>
   );
 }
